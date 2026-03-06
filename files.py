761e6fae408b5aa7cc974e84398a136dc7d1bb2c
Regan4444 <105506212+Regan4444@users.noreply.github.com>
2026-03-06 09:23:45 -0600

    Update files.py

--- files.py
+++ files.py
@@ -1,8 +1,8 @@
 import requests
 
-DOMAIN = "https://grayson.instructure.com"
-ADMIN_TOKEN = "4480~PturLY3URL2MCx9vzthmHUU488c4XDEcKRkU27mzUXPGT4M9uP2HmGZ6K79xTzC7".strip()
-AUTO_USER_ID = 70952  # Canvas ID of the automation user
+DOMAIN = "paste domain here"
+ADMIN_TOKEN = "paste token here".strip()
+AUTO_USER_ID = 00000  # Canvas ID of the automation user
 
 url = f"{DOMAIN}/api/v1/users/{AUTO_USER_ID}/tokens"
 params = {"access_token": ADMIN_TOKEN}  # <-- sends token in URL
@@ -14,4 +14,5 @@ data = {
 
 r = requests.post(url, params=params, data=data, timeout=30)
 print(r.status_code)
+
 print(r.text[:1000])
