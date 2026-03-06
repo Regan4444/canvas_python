a4fb3034df46009bd2719dc9e7f398e8162d60f6
Regan4444 <105506212+Regan4444@users.noreply.github.com>
2026-03-06 09:29:59 -0600

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,7 +1,7 @@
 import requests
 
-DOMAIN = "https://grayson.instructure.com"
-TOKEN = "4480~YmPPBYnTQzPUDt6nnekAwrkm2Ye64MM3KUYUQQ4fyUf6k2kD4zGETfwUMAZR3wzW"
+DOMAIN = "paste domain here"
+TOKEN = "paste token here"
 
          
 
@@ -9,4 +9,5 @@ headers = {"Authorization": f"Bearer {TOKEN}"}
 r = requests.get(f"{DOMAIN}/api/v1/users/self", headers=headers)
 print(r.status_code)
 print(r.text[:500])
+
 
